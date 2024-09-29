85d1c13c6ed39448bffc20272fc3e8a74868b248
Jesse Quier <jquie803@live.kutztown.edu>
2024-09-29 19:02:09 -0400

    updates to TCPServer2

--- TCPClient2.py
+++ TCPClient2.py
@@ -7,19 +7,38 @@ def main():
     clientSocket.connect((serverName, serverPort))
     print('\n Connect to:', '127.0.0.1, 16666')
     while True:
-        intro = input('Would you like to play a game of black jack:')   # asks user if they want to play
+        intro = input('Would you like to play a game of black jack: ')   # asks user if they want to play
         clientSocket.send(intro.encode()) # sends users answer
         if intro.lower() == "no": # gets us out of game if users says no
             print("Exiting the game.") 
             break
-        reply = clientSocket.recv(1024).decode() # receives the reply could be one of three
-        print(reply)
+        start_reply = clientSocket.recv(1024).decode() # receives the reply could be one of three
+        print(start_reply)
         
-        message = clientSocket.recv(1024).decode()
-        if not message:
-            break
-        print(message)
-        break
+        deal_message = clientSocket.recv(1024).decode()
+        print(deal_message)
+
+        flag = clientSocket.recv(1024).decode()
+        while flag == 'True':
+            hit_message = input('Hit / Stay: ')
+            clientSocket.send(hit_message.encode())
+            #ace_message = clientSocket.recv(1024).decode()
+        if flag == 'False':
+            message = clientSocket.recv(1024).decode()
+            print(message)
+        elif flag == 'Ace':
+            ace_choice = input('Choose value of Ace (1 / 11): ')
+            clientSocket.send(ace_choice.encode())
+            
+        
+        
+
+        #hit_message = input('Hit / Stay: ')
+        #clientSocket.send(hit_message.encode())
+
+        reply = clientSocket.recv(1024).decode()
+        print(reply)
+
     clientSocket.close()
 
 if __name__ == '__main__':
--- TCPServer2.py
+++ TCPServer2.py
@@ -11,18 +11,188 @@ def addCards(cards):
     cardSum = sum(values(val) for val in cards)
     return cardSum
 
-#def moreCardsPlayer():
+def hit(socket, deck, p_card1, p_card2, p_sum, d_card1, d_card2, d_score, d_sum):
+    new_card = (deck.pop())
+    new_sum = values(socket, new_card) + p_sum
+    #return (new_card, new_sum)
     
+    message = [
+    "----------------------------", "\n",
+    "Player shows: ", p_card1, p_card2, new_card,"\n",
+    "Player score: ", new_sum, "\n",
+    "Dealer shows: ", d_card1, "****", "\n",
+    "Dealer score: ", d_score, "\n",
+    "----------------------------", "\n"
+    ]
+    message_str = " ".join(map(str, message))
+    socket.send(message_str.encode())
+    if new_sum > 21:
+        
+        print("----------------------------")
+        print("Player shows: ", p_card1, p_card2, new_card)
+        print("Player score: ", new_sum)
+        print("Dealer shows: ", d_card1, d_card2)
+        print("Dealer score: ", d_sum)
+        print("----------------------------")
+        print('Player busted, Dealer wins\n')
+        message = [
+            "----------------------------", "\n",
+            "Player shows: ", p_card1, p_card2, new_card,"\n",
+            "Player score: ", new_sum, "\n",
+            "Dealer shows: ", d_card1, d_card2, "\n",
+            "Dealer score: ", d_sum, "\n",
+            "----------------------------", "\n"
+            "\nPlayer busted, Dealer wins\n"
+        ]
+        lose_message = " ".join(map(str, message))
+        socket.send(lose_message.encode())
+        socket.close()
+        exit()
+    elif new_sum == 21:
+        print('\nBlackjack! Player wins\n')
+        message = [
+            "----------------------------", "\n",
+            "Player shows: ", p_card1, p_card2, new_card,"\n",
+            "Player score: ", new_sum, "\n",
+            "Dealer shows: ", d_card1, d_card2, "\n",
+            "Dealer score: ", d_sum, "\n",
+            "----------------------------", "\n"
+            "\nBlackjack! Player wins\n"
+        ]
+        win_message = " ".join(map(str, message))
+        socket.send(win_message.encode())
+        socket.close()
+        exit()
+    else:
+        print("----------------------------")
+        print("Player shows: ", p_card1, p_card2, new_card)
+        print("Player score: ", new_sum)
+        print("Dealer shows: ", d_card1, d_card2)
+        print("Dealer score: ", d_sum)
+        print("----------------------------")
+        hit_or_stay(socket, deck, p_card1, p_card2, new_sum, d_card1, d_card2, d_score, d_sum)
+
+
+def stay(socket, deck, p_card1, p_card2, p_sum, d_card1, d_card2, d_score, d_sum):
+    while d_sum < 17:
+        new_card = (deck.pop())
+        new_sum = values(socket, new_card) + d_sum
+ 
+        message = [ "\n\n Dealer hit: \n"
+            "----------------------------", "\n",
+            "Player shows: ", str(p_card1), str(p_card2),"\n",
+            "Player score: ", p_sum, "\n",
+            "Dealer shows: ", str(d_card1), str(d_card2), "\n",
+            "Dealer score: ", new_sum, "\n",
+            "----------------------------", "\n"
+        ]
+        message_str = " ".join(map(str, message))
+        socket.send(message_str.encode())
+        if 21 >= new_sum > p_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2, new_card)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", new_sum)
+            print("----------------------------")
+            print('\nDealer wins\n')
+            socket.close()
+            exit()
+        elif 21 >= p_sum > new_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2, new_card)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", new_sum)
+            print("----------------------------") 
+            print('\nPlayer wins\n')
+            socket.close()
+            exit()
+        elif p_sum == new_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2, new_card)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", new_sum)
+            print("----------------------------")
+            print('\nPush\n')
+            socket.close()
+            exit()
+    if d_sum >= 17:
+        if 21 >= d_sum > p_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", d_sum)
+            print("----------------------------")
+            print('\nDealer wins\n')
+            socket.close()
+            exit()
+        elif 21 >= p_sum > d_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", d_sum)
+            print("----------------------------") 
+            print('\nPlayer wins\n')
+            socket.close()
+            exit()
+        elif p_sum == d_sum:
+            print("----------------------------")
+            print("Player shows: ", p_card1, p_card2)
+            print("Player score: ", p_sum)
+            print("Dealer shows: ", d_card1, d_card2)
+            print("Dealer score: ", d_sum)
+            print("----------------------------")
+            print('\nPush\n')
+            socket.close()
+            exit()
+
+def hit_or_stay(socket, deck, p_card1, p_card2, p_sum, d_card1, d_card2, d_score, d_sum):
+    flag = 'True'
+    socket.send(flag.encode())
+    hit_message = socket.recv(1024).decode()
+    if hit_message == 'hit':
+        print('\nPlayer hit.\n')
+        hit(socket, deck, p_card1, p_card2, p_sum, d_card1, d_card2, d_score, d_sum)
+    elif hit_message == 'stay':
+        flag = 'False'
+        print('\nPlayer stood\n')
+        stay(socket, deck, p_card1, p_card2, p_sum, d_card1, d_card2, d_score, d_sum)
+#def moreCardsPlayer():
+
 #def moreCardsDealer():
       
 
-def values(val):
-	if val[0] in ['Jack', 'Queen', 'King']:
-		return 10
-	elif val[0] == 'Ace':
-		return 11
-	else:
-		return int(val[0]) 
+def values(socket, val):
+    if val[0] in ['Jack', 'Queen', 'King']:
+        return 10
+    elif val[0] == 'Ace':
+        while True:  # Loop until valid input
+            try:
+                ace_choice = socket.recv(1024).decode()
+                if ace_choice == '1':
+                    return 1
+                elif ace_choice == '11':
+                    return 11
+                else:
+                    print("Invalid choice. Please enter 1 or 11.")
+            except ValueError:
+                print("Please enter a valid number (1 or 11).")
+        '''flag = 'Ace'
+        socket.send(flag.encode())
+        reply = socket.recv(1024).decode()
+        if reply == '1':
+            return 1
+        elif reply == '11':
+            return 11
+        else:
+            return None''' 
+    else:
+        return int(val[0])
+ 
 
 
 def main():
@@ -33,14 +203,14 @@ def main():
     serverSocket.listen(5)
     print('The server is ready to receive')
     while True:
-        connectionSocket,addr = serverSocket.accept()
+        connectionSocket, addr = serverSocket.accept()
         sentence = connectionSocket.recv(1024).decode()
         if 'yes' == sentence:
             reply = "Okay lets play a game.\n"
             connectionSocket.send(reply.encode())
             dealersDeck = makeDeck()
             random.shuffle(dealersDeck)
-            playerCards = []
+            '''playerCards = []
             dealerCards = []
             playerCards.append(dealersDeck.pop())
             dealerCards.append(dealersDeck.pop())
@@ -48,8 +218,22 @@ def main():
             dealerCards.append(dealersDeck.pop())
             playerSum = addCards(playerCards)
             dealerSum = addCards(dealerCards)
-            print (playerCards)
-            print (playerSum)
+            print("----------------------------")
+            print("Player shows: ", playerCards)
+            print("Player score: ", playerSum)
+            dealerCards.pop()
+            print("Dealer shows: ", dealerCards)
+            print("Dealer score: ", dealerSum)
+            print("----------------------------")
+
+            message = [
+                "----------------------------", "\n",
+                "Player shows: ", str(playerCards),"\n",
+                "Player score: ", playerSum, "\n",
+                "Dealer shows: ", str(dealerCards), "****", "\n",
+                "Dealer score: ", dealerSum, "\n",
+                "----------------------------", "\n"
+            ]'''
             player_card1 = [dealersDeck.pop()]
             #p_card_val1 = (values(val) for val in player_card1)
             dealer_card1 = [dealersDeck.pop()]
@@ -57,9 +241,9 @@ def main():
             #p_card_val2 = (values(val) for val in player_card2)
             dealer_card2 = [dealersDeck.pop()]
 
-            player_sum = sum(values(val) for val in player_card1 + player_card2)
-            dealer_sum = sum(values(val) for val in dealer_card1 + dealer_card2)
-            dealer_score = sum(values(val) for val in dealer_card1)
+            player_sum = sum(values(connectionSocket, val) for val in player_card1 + player_card2)
+            dealer_sum = sum(values(connectionSocket, val) for val in dealer_card1 + dealer_card2)
+            dealer_score = sum(values(connectionSocket, val) for val in dealer_card1)
             
 
             print("----------------------------")
@@ -79,12 +263,20 @@ def main():
             ]
 
             message_str = " ".join(map(str, message))
-
             connectionSocket.send(message_str.encode())
-            connectionSocket.close()  
-        
+
+            if player_sum < 21:
+                hit_or_stay(connectionSocket, dealersDeck, player_card1, player_card2, player_sum, dealer_card1, dealer_card2, dealer_score, dealer_sum)
+            elif player_sum == 21:
+                flag = 'False'
+                connectionSocket.send(flag.encode())
+                print('\nBlackjack! Player wins\n')
+                win_message = ('\nBlackjack! Player wins\n')
+                connectionSocket.send(win_message.encode())
+                connectionSocket.close()
+                exit()
         elif 'no' == sentence:
-            message = 'To bad I would have won.'
+            message = 'Too bad I would have won.'
             connectionSocket.send(message.encode())
         else:
             message = 'Invalid input.'
