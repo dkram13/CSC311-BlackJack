5f3bd6feb3bdd5ecf0e873316124c227d51548b8
dkram150 <151053698+dkram13@users.noreply.github.com>
2024-09-30 01:56:35 -0400

    changed the Idea will talk about tomorrow
    cut down alot of the space taken up by certain parts of server and still kept the functionality to a degree still working at figuring out how to do certain parts.

--- TCPClient.py
+++ TCPClient.py
@@ -1,26 +1,46 @@
 from socket import *
-serverName = '127.0.0.1'
-serverPort = 16666
-clientSocket = socket(AF_INET, SOCK_STREAM)
 
-clientSocket.connect((serverName, serverPort))
-print('\n Connect to:', '127.0.0.1, 16666')
-while True:
-	intro = input('Would you like to play a game of black jack:')
-	clientSocket.send(intro.encode())
-	if intro.lower() == "no":
-		print("Exiting the game.")
-		break
-	reply = clientSocket.recv(1024).decode()
-	print(reply)
-	
-	message = clientSocket.recv(1024).decode()
-	if not message:
-		break
-	print(message)
-	break
-clientSocket.close()
+def main():
+	serverName = '127.0.0.1'
+	serverPort = 16666
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
+	print('\n Connect to:', '127.0.0.1, 16666')
 
+	while True:
+		intro = input('Would you like to play a game of black jack: ')  # asks user if they want to play
+		clientSocket.send(intro.encode())  # sends user's answer
+		if intro.lower() == "no":  # gets us out of game if user says no
+			print("Exiting the game.")
+			break
+		
+		start_reply = clientSocket.recv(1024).decode()  # receives confirmation of game
+		print(start_reply)
+		heWon = clientSocket.recv(1024).decode()
+		if "I won before" in heWon:
+			print(heWon)
+			break
+		else:
+			print(heWon)
+		iWannaPlay = True
+
+		while iWannaPlay:
+			deal_message = clientSocket.recv(1024).decode()  # receives the card draw message from server
+			hitOrStay = input(deal_message)  # asks user to hit or stay
+			clientSocket.send(hitOrStay.encode())  # sends user's answer to the server
+			
+			if hitOrStay.lower() == "stay":
+				print (clientSocket.recv(1024).decode())
+				iWannaPlay = False  # breaks the loop if the user chooses to stay
+
+		play_again = input("Would you like to play another game? (yes/no): ")  # Ask if they want to play again
+		if play_again.lower() != "yes":
+			print("Exiting the game.")
+			break	
+	clientSocket.close()
+
+if __name__ == '__main__':
+    main()
 ## Server sends cards and asks to Hit or Stay
 ## if the dealer is dealt 21 games over right away
 ## server starts and get the cards ready to send
--- TCPServer.py
+++ TCPServer.py
@@ -1,65 +1,124 @@
 from socket import *
 import random
-serverPort = 16666
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(5)
-print('The server is ready to receive')
 
-card_suits = ["Hearts", 'Diamonds', 'Spades', 'Clubs']
-card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-deck = [(val, suit) for val in card_values for suit in card_suits]
+def makeDeck():
+    card_suits = ["Hearts", 'Diamonds', 'Spades', 'Clubs']
+    card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    deck = [(val, suit) for val in card_values for suit in card_suits]   
+    return deck
+
+def addCards(cards):
+    cardSum = sum(values(val) for val in cards)
+    return cardSum
+
+def playerHitOrStay(playerCards, playerSum, dealerCards, dealerSum, dealer_score, connectionSocket, dealersDeck):
+    gameGoesOn = True
+    while gameGoesOn:
+        # Show current game status
+        print("----------------------------")
+        print("Player shows: ", playerCards)
+        print("Player score: ", playerSum)
+        print("Dealer shows: ", dealerCards)
+        print("Dealer score: ", dealerSum)
+        print("----------------------------")
+
+        # Prepare the message to send to the client
+        message = [
+            "----------------------------", "\n",
+            "Player shows: ", str(playerCards), "\n",
+            "Player score: ", playerSum, "\n",
+            "Dealer shows: ", str(dealerCards[0]), "****", "\n",
+            "Dealer score: ", dealer_score, "\n",
+            "----------------------------", "\n",
+            "Would you like to Hit or Stay?\n"  
+        ]
+
+        message_str = " ".join(map(str, message))
+        connectionSocket.send(message_str.encode())  # sends the card draw
+
+        # Receive player's answer (hit or stay)
+        answer = connectionSocket.recv(1024).decode()
+
+        if answer.lower() == "stay":
+            gameGoesOn = False
+            reply = "Player stayed. Game over!"
+            connectionSocket.send(reply.encode())
+        
+        elif answer.lower() == "hit":
+            playerCards.append(dealersDeck.pop())
+            playerSum = addCards(playerCards)
+            
+            if playerSum > 21:
+                reply = "Player busts! Game over."
+                print(reply)
+                connectionSocket.send(reply.encode())
+                gameGoesOn = False
+#def dealerHitOrStay(dealerCards, dealerSum, dealer_score, connectionSocket, dealersDeck):
+	#while True:
+    
+#def moreCardsDealer():
+      
 
 def values(val):
 	if val[0] in ['Jack', 'Queen', 'King']:
 		return 10
-	elif val[0] == 'Ace':
+	elif val[0] == 'Ace':	
 		return 11
 	else:
 		return int(val[0]) 
 
-while True:
-	connectionSocket,addr = serverSocket.accept()
 
-	random.shuffle(deck)
-	player_card1 = [deck.pop()]
-	p_card_val1 = (values(val) for val in player_card1)
-	dealer_card1 = [deck.pop()]
-	player_card2 = [deck.pop()]
-	p_card_val2 = (values(val) for val in player_card2)
-	dealer_card2 = [deck.pop()]
+def main():
+	print("Starting server...")
+	serverPort = 16666
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket.bind(('', serverPort))
+	serverSocket.listen(5)
+	print('The server is ready to receive')
 
-	player_sum = sum(values(val) for val in player_card1 + player_card2)
-	dealer_sum = sum(values(val) for val in dealer_card1 + dealer_card2)
-	dealer_score = sum(values(val) for val in dealer_card1)
-	
+	while True:
+		connectionSocket, addr = serverSocket.accept()
+		while True:
+			sentence = connectionSocket.recv(1024).decode()  # yes or no
 
-	print("----------------------------")
-	print("Player shows: ", player_card1, player_card2)
-	print("Player score: ", player_sum)
-	print("Dealer shows: ", dealer_card1, "****")
-	print("Dealer score: ", dealer_score)
-	print("----------------------------")
+			if 'yes' == sentence:
+				reply = "Okay, let's play a game.\n"
+				connectionSocket.send(reply.encode())  # sends confirmation 
 
-	message = [
-		"----------------------------", "\n",
-		"Player shows: ", str(player_card1), str(player_card2),"\n",
-		"Player score: ", player_sum, "\n",
-		"Dealer shows: ", str(dealer_card1), "****", "\n",
-		"Dealer score: ", dealer_score, "\n",
-		"----------------------------", "\n"
-	]
+				dealersDeck = makeDeck()
+				random.shuffle(dealersDeck)
 
-	message_str = " ".join(map(str, message))
+				playerCards = []
+				dealerCards = []
 
-	connectionSocket.send(message_str.encode())
-	connectionSocket.close()
-#	sentence = int(connectionSocket.recv(1024).decode())
-#	if rn == sentence:
-#		message = 'correct'
-#			connectionSocket.send(message.encode())
-#			connectionSocket.close()
-#		else:
-#			message = 'incorrect'
-#			connectionSocket.send(message.encode())
-	
+				# Deal initial cards
+				playerCards.append(dealersDeck.pop())
+				dealerCards.append(dealersDeck.pop())
+				playerCards.append(dealersDeck.pop())
+				dealerCards.append(dealersDeck.pop())
+
+				playerSum = addCards(playerCards)
+				dealerSum = addCards(dealerCards)
+				dealer_score = values(dealerCards[0])
+				if dealerSum == 21:
+					youLose = "I won before the game even started to bad so sad.\n"
+					connectionSocket.send(youLose.encode())  # sends confirmation 
+					continue
+				else:
+					youLose = "I didn't win right way its go time.\n"
+					connectionSocket.send(youLose.encode())  # sends confirmation 
+				
+				playerHitOrStay(playerCards, playerSum, dealerCards, dealerSum, dealer_score, connectionSocket, dealersDeck)
+				
+			elif 'no' == sentence:
+				message = 'To bad I would have won.'
+				connectionSocket.send(message.encode())
+			else:
+				message = 'Invalid input.'
+				connectionSocket.send(message.encode())
+        
+		connectionSocket.close()  # Close the connection after the game            
+
+if __name__ == '__main__':
+    main()
+
